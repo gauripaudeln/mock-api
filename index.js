4a8c5bcea72a15ce9638df739e00341284b80244
Gauri Paudel <gauri.paudeln@gmail.comm>
2021-06-12 19:18:57 -0500

    add clients

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const categories = [
 
 const products  = [
   {"ProductID": 1, "ProductName": "Tea", "CategoryID": 1, "QuantityPerUnit": "10 boxes x 20 bags", "UnitPrice": 18.0, "UnitsInStock": 39, "Discontinued": false },
-  {"ProductID": 7, "ProductName": "Dried Pears", "CategoryID": 7, "QuantityPerUnit": "12 - 1 lb pkgs.", "UnitPrice": 30.0, "UnitsInStock": 15, "Discontinued": false },
+  {"ProductID": 7, "ProductName": "Dried Pears", "CategoryID": 1, "QuantityPerUnit": "12 - 1 lb pkgs.", "UnitPrice": 30.0, "UnitsInStock": 15, "Discontinued": false },
   {"ProductID": 8, "ProductName": "Cranberry Sauce", "CategoryID": 2, "QuantityPerUnit": "12 - 12 oz jars", "UnitPrice": 40.0, "UnitsInStock": 6, "Discontinued": false },
   {"ProductID": 9, "ProductName": "Mishi Kobe Niku", "CategoryID": 6, "QuantityPerUnit": "18 - 500 g pkgs.", "UnitPrice": 97.0, "UnitsInStock": 29, "Discontinued": true },
   {"ProductID": 14, "ProductName": "Tofu", "CategoryID": 7, "QuantityPerUnit": "40 - 100 g pkgs.", "UnitPrice": 23.25, "UnitsInStock": 35, "Discontinued": false },
@@ -28,6 +28,45 @@ const products  = [
   {"ProductID": 72, "ProductName": "Mozzarella di Giovanni", "CategoryID": 4, "QuantityPerUnit": "24 - 200 g pkgs.", "UnitPrice": 34.8, "UnitsInStock": 14, "Discontinued": false},
   {"ProductID": 73,"ProductName": "Röd Kaviar", "CategoryID": 8, "QuantityPerUnit": "24 - 150 g jars", "UnitPrice": 15.0, "UnitsInStock": 101, "Discontinued": true }
 ];
+
+
+const clients  = [
+  {"Name": "Google", "Year": 2015, "ContractAmount": 1500000},
+  {"Name": "Google", "Year": 2016, "ContractAmount": 2000000},
+  {"Name": "Google", "Year": 2019, "ContractAmount": 50000000},
+  
+  {"Name": "Microsoft", "Year": 2015, "ContractAmount": 2000000},
+  {"Name": "Microsoft", "Year": 2016, "ContractAmount": 2234444},
+  {"Name": "Microsoft", "Year": 2020, "ContractAmount": 50000000},
+
+  {"Name": "Amazon", "Year": 2015, "ContractAmount": 1000000},
+  {"Name": "Amazon", "Year": 2016, "ContractAmount": 2234444},
+  {"Name": "Amazon", "Year": 2020, "ContractAmount": 50000000},
+
+
+  {"Name": "Facebook", "Year": 2015, "ContractAmount": 1000000},
+  {"Name": "Facebook", "Year": 2016, "ContractAmount": 2234444},
+  {"Name": "Facebook", "Year": 2020, "ContractAmount": 50000000},
+
+  {"Name": "SpaceX", "Year": 2015, "ContractAmount": 6000000},
+  {"Name": "SpaceX", "Year": 2016, "ContractAmount": 70000000},
+  {"Name": "SpaceX", "Year": 2020, "ContractAmount": 80000000},
+
+  {"Name": "Oracle", "Year": 2015, "ContractAmount": 10000},
+  {"Name": "Oracle", "Year": 2016, "ContractAmount": 50000},
+  {"Name": "Oracle", "Year": 2020, "ContractAmount": 40000},
+
+  {"Name": "Apple", "Year": 2015, "ContractAmount": 4000000},
+  {"Name": "Apple", "Year": 2016, "ContractAmount": 6000000},
+  {"Name": "Apple", "Year": 2020, "ContractAmount": 8000000},
+
+
+  {"Name": "Dell", "Year": 2015, "ContractAmount": 30000},
+  {"Name": "Dell", "Year": 2016, "ContractAmount": 85000},
+  {"Name": "Dell", "Year": 2020, "ContractAmount": 500000}
+  
+];
+
 
 
 var express = require('express');
@@ -47,6 +86,10 @@ server.get('/products',(req,res)=> {
 
 server.get('/categories',(req,res)=> {
   res.send(categories);
+}); 
+
+server.get('/clients',(req,res)=> {
+  res.send(clients);
 }); 
 
 server.listen(8080,()=>{
